1711b0dd09511cc69d48e663c9f6cf8f5d84dae7
Merlakre <corentin.becuhet@hotmail.fr>
2019-09-23 16:15:28 -0400

    On enlève les commentaires

--- app/src/main/java/com/uqac/mobile/roadtripplanner/MapsActivity.java
+++ app/src/main/java/com/uqac/mobile/roadtripplanner/MapsActivity.java
@@ -14,8 +14,6 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-//test
-    //retest2
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
