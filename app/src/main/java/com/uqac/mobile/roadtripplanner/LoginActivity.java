6b5472e573d5c2171ef550ff003b8d1b30c450ae
Theo Gaerel <theo@gaerel.net>
2019-09-25 17:56:25 -0400

    avec le xml c'est mieux

--- app/src/main/java/com/uqac/mobile/roadtripplanner/LoginActivity.java
+++ app/src/main/java/com/uqac/mobile/roadtripplanner/LoginActivity.java
@@ -1,4 +1,30 @@
 package com.uqac.mobile.roadtripplanner;
 
-public class LoginActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Définissez votre vue, rien de plus. Tout sera pris en charge par le fragment qui affiche les données
+        setContentView(R.layout.login_activity_layout);
+
+        /*
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.activity_main_frame_layout, fragment);
+        transaction.commit();*/
+    }
+
+    public void SkipLogin(View v)
+    {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/com/uqac/mobile/roadtripplanner/LoginFragment.java
+++ app/src/main/java/com/uqac/mobile/roadtripplanner/LoginFragment.java
@@ -1,6 +1,18 @@
 package com.uqac.mobile.roadtripplanner;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import androidx.fragment.app.Fragment;
 
 public class LoginFragment extends Fragment {
+    View view;
+
+    @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.login_fragment_layout, container, false);
+        this.view = view;
+        return view;
+    }
 }
